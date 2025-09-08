53b068f52de3f87b83fc8976cf803cc87115630c
stellaLily0819 <iridescentcloud03@gmail.com>
2025-09-09 01:18:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,21 +9,23 @@ st.set_page_config(page_title="Dmg Graph", layout="centered")
 st.title("실시간 데미지 계산 그래프")
 
 # z 계산 함수
-def compute_z(x, y, atk, defense, w):
+def compute_z(x, y, atk, defense, w, multiplier):
     numerator = atk ** 2
     denominator = atk + defense * (1 - w * 0.01)
-    return (numerator / denominator) * (1 + x * 0.01) * 1.2 * (100 * 0.01) * (y * 0.01)
+    return (numerator / denominator) * (1 + x * 0.01) * multiplier * (100 * 0.01) * (y * 0.01)
 
 # 슬라이더
 atk = st.slider("🗡 Attack", 1, 8000, 1000, step=50)
 defense = st.slider("🛡 Enemy Defense", 1, 7000, 1000, step=50)
-w = st.slider("💥 Weakening %", 0.0, 100.0, 50.0, step=1.0)
-x = st.slider("📊 Damage Increase %", 0.0, 400.0, 100.0, step=5.0)
-y = st.slider("🎯 Crit %", 0.0, 400.0, 100.0, step=5.0)
+w = st.slider("방어감소 %", 0.0, 100.0, 50.0, step=1.0)
+x = st.slider("피해증가 %", 0.0, 400.0, 100.0, step=5.0)
+y = st.slider("치명피해 %", 0.0, 400.0, 100.0, step=5.0)
+multiplier = st.radio("약점 계수:", [1.0, 1.1, 1.2], index=0, horizontal=True)
+st.write(f"선택한 계수 값: {multiplier}")
 
 # z 값 계산
 z_val = compute_z(x, y, atk, defense, w)
-st.markdown(f"### ✅ 실제 데미지 (z): `{z_val:.2f}`")
+st.markdown(f"###실제 데미지 (z): `{z_val:.2f}`")
 
 # 3D
 x_vals = np.linspace(0, 400, 50)
